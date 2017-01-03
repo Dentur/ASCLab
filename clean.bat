23a71e3285bbae367901e5d9be927aed2d74e5c7
Okan0 <Tim1506@gmx.net>
2017-01-03 14:46:50 +0100

    Batch erstellt, welche alle überflüssigen Dateien löscht

---
+++ clean.bat
@@ -0,0 +1,33 @@
+@echo off
+for %%i in (*.l) do (
+    echo "Delete %%i files..."
+	IF EXIST "%%~ni.c" (
+		del "%%~ni.c"
+	)
+	IF EXIST "%%~ni.h" (
+		del "%%~ni.h"
+	)
+	IF EXIST "%%~ni.v" (
+		del "%%~ni.v"
+	)
+)
+for %%i in (*.y) do (
+	echo "Delete %%i files..."
+	IF EXIST "%%~ni.c" (
+		del "%%~ni.c"
+	)
+	IF EXIST "%%~ni.h" (
+		del "%%~ni.h"
+	)
+	IF EXIST "%%~ni.v" (
+		del "%%~ni.v"
+	)
+)
+for %%i in (*.exe) do (
+	echo "Delete %%i..."
+	del "%%i"
+)
+echo "Delete lex.yy.c..."
+IF EXIST "lex.yy.c" (
+	del "lex.yy.c"
+)
